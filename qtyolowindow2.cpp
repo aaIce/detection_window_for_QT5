18c62d3d52ab79d8d94233142a4fc945a947b898
aaIce <131343484+aaIce@users.noreply.github.com>
2023-11-29 19:49:36 +0800

    Update and rename qtyolowindow.c to qtyolowindow2.cpp

--- qtyolowindow2.cpp
+++ qtyolowindow2.cpp
@@ -1,11 +1,12 @@
-#include "qtyolowindow.h"
+#include "qtyolowindow2.h"
 using namespace cv;
 bool caiji_flag = 0;
 bool detect_flag = 0;
 bool save_flag = 0;
+extern std::mutex mtx;
 //构造函数
 QTyoloWindow::QTyoloWindow(QWidget *parent)
-    : QMainWindow(parent)
+	: QMainWindow(parent)
 {
 	ui.setupUi(this);//界面初始化
 	//链接清空按钮，清空状态栏
@@ -26,12 +27,12 @@ QTyoloWindow::QTyoloWindow(QWidget *parent)
 	connect(ui.start_dec_btn_image, SIGNAL(clicked()), this, SLOT(bp_detoneimg_clicked()));
 	//测试代码
 	ui.textBrowser->append("welcome93");
-	cv::Mat img0 = cv::imread("C:/Users/tianmingYun/Desktop/cvimage.jpg");
+	cv::Mat img0 = cv::imread("C:/Users/DELL/Desktop/1.jpg");
 	cv::cvtColor(img0, img0, COLOR_BGR2RGB);
 	QImage Qtemp = QImage((const unsigned char*)(img0.data), img0.cols, img0.rows, img0.step, QImage::Format_RGB888);
 	ui.label->setPixmap(QPixmap::fromImage(Qtemp));
 	ui.label->setScaledContents(1);
-	
+
 	cam = new DahengCamera();//实例化相机
 
 	IsCamOpen_flag = 0;//相机成功打开标志位 未打开0 打开1
@@ -41,19 +42,18 @@ QTyoloWindow::QTyoloWindow(QWidget *parent)
 	IsDetoneimg_flg = 0;//检测单张图片标志位 未检测0，检测1
 
 	//创建定时器 刷新图片
-	//timer = new QTimer(this);
-	//connect(timer, SIGNAL(timeout()), this, SLOT(timerTimeout()));
-	//timer->setInterval(48); // 设置定时器间隔为0.48秒
+	timer = new QTimer(this);
+	connect(timer, SIGNAL(timeout()), this, SLOT(timerTimeout()));
+	timer->setInterval(48); // 设置定时器间隔为0.48秒
 
 	//创建一个线程用于检测与刷新
-	img_thread = new ImageThread();
-	//connect(img_thread, SIGNAL(detectSignal(const QImage&)), this, SLOT(refreshimage(const QImage&)));
-	connect(img_thread, &ImageThread::detectSignal, this, &QTyoloWindow::refreshimage);
+	//img_thread = new ImageThread();
+	//connect(img_thread, &ImageThread::detectSignal, this, &QTyoloWindow::refreshimage);
 }
 QTyoloWindow::~QTyoloWindow()
 {
 	delete cam;//释放堆上的内存
-	//delete timer;
+	delete timer;
 }
 //清空状态栏槽函数
 void QTyoloWindow::bp_clearMessage_clicked()
@@ -82,8 +82,8 @@ void QTyoloWindow::bp_openCam_clicked()
 }
 //关闭相机
 void QTyoloWindow::bp_closeCam_clicked()
-{	
-	if ((IsCamOpen_flag == 1)&&(IsCamAcq_flag==0))//只有相机打开并且结束采集才能关闭相机
+{
+	if ((IsCamOpen_flag == 1) && (IsCamAcq_flag == 0))//只有相机打开并且结束采集才能关闭相机
 	{
 		cam->closeCam_fuc();
 		IsCamOpen_flag = 0;//相机关闭，标志位置0
@@ -93,18 +93,16 @@ void QTyoloWindow::bp_closeCam_clicked()
 }
 //开始采集
 void QTyoloWindow::bp_startAcq_clicked()
-{	
+{
 	if (IsCamOpen_flag == 1)//如果相机打开了才能采集
 	{
 		cam->StartAcquisition();
 		IsCamAcq_flag = 1;//开始采集
 		caiji_flag = 1;
-		//timer->start();//开启定时器
+		timer->start();//开启定时器
 
-		//创建一个线程用于检测与刷新
-		//img_thread = new ImageThread();
-		//connect(img_thread, SIGNAL(detectSignal(const cv::Mat&)), this, SLOT(refreshimage(const cv::Mat&)));
-		img_thread->start();
+		//创建一个线程用于检测与刷新  线程启动
+		//img_thread->start();
 	}
 	else
 		QMessageBox::critical(nullptr, "Error", "Camera is not opened!");
@@ -127,9 +125,11 @@ void QTyoloWindow::timerTimeout()
 {
 	if (!rgb_img.empty())//如果对象不为空
 	{	
+		mtx.lock();
 		cv::Mat image_ = rgb_img.clone();
+		mtx.unlock();
 		//刷新采集到的图
-		if ((IsCamAcq_flag == 1)&&(IsDetect_flag == 0))//开始采集且没有检测
+		if ((IsCamAcq_flag == 1) && (IsDetect_flag == 0))//开始采集且没有检测
 		{
 			if (IsSaveimg_flag == 1)//如果保存图片
 			{
@@ -150,9 +150,6 @@ void QTyoloWindow::timerTimeout()
 			ui.label->setPixmap(QPixmap::fromImage(Qtemp));
 			ui.label->setScaledContents(1);
 		}
-		//QImage Qtemp = QImage((const unsigned char*)(image_.data), image_.cols, image_.rows, image_.step, QImage::Format_RGB888);
-		//ui.label->setPixmap(QPixmap::fromImage(Qtemp));
-		//ui.label->setScaledContents(1);
 	}
 }
 void QTyoloWindow::bp_startDet_clicked()
@@ -200,16 +197,16 @@ void QTyoloWindow::bp_openimg_clicked()
 	// 打开本地文件对话框并选择一个文件
 	QString fileName = QFileDialog::getOpenFileName(nullptr, "choose a file", "", "all files (*.*)");
 	// 如果用户选择了文件
-	if (!fileName.isEmpty()) 
+	if (!fileName.isEmpty())
 	{
 		filePathStr = fileName.toStdString();
 		cv::Mat img2 = cv::imread(filePathStr);
 		if (!img2.empty())
-		{	
+		{
 			QString str = "The fielpath is %1";
 			QString result = str.arg(fileName);
 			ui.textBrowser_image->append(result);//路径显示到状态栏
-		
+
 			cv::cvtColor(img2, img2, COLOR_BGR2RGB);
 			QImage Qtemp = QImage((const unsigned char*)(img2.data), img2.cols, img2.rows, img2.step, QImage::Format_RGB888);
 			ui.label_image->setPixmap(QPixmap::fromImage(Qtemp));
@@ -221,7 +218,7 @@ void QTyoloWindow::bp_openimg_clicked()
 			QMessageBox::critical(nullptr, "Error", "please open a picture!");
 		}
 	}
-	else 
+	else
 	{
 		QMessageBox::critical(nullptr, "Error", "no file!");
 	}
@@ -238,14 +235,17 @@ void QTyoloWindow::bp_detoneimg_clicked()
 	}
 }
 
+//检测 刷新 线程用的函数
 void ImageThread::run()
 {
 	QImage Qtemp_image_;
 	while (caiji_flag)
-	{
+	{	
 		if (!rgb_img.empty())//如果对象不为空
 		{
-			cv::Mat image_ = rgb_img.clone();
+			mtx.lock();
+			cv::Mat image_ = rgb_img;
+			mtx.unlock();
 			//刷新采集到的图
 			if ((caiji_flag == 1) && (detect_flag == 0))//开始采集且没有检测
 			{
@@ -261,6 +261,7 @@ void ImageThread::run()
 				//检测代码
 			}
 			Qtemp_image_ = QImage(image_.data, image_.cols, image_.rows, image_.step, QImage::Format_RGB888).copy();
+			std::this_thread::sleep_for(1ms);
 			emit detectSignal(Qtemp_image_);
 		}
 	}
